0583e2a603cc879452f5516b06af917d8c522e5c
AbdTll <abdalaziztll992010@gmail.com>
2024-11-09 06:04:35 -0800

    function

--- main.dart
+++ main.dart
@@ -1,271 +1,88 @@
-main() {
-  // declaration
-  int? coutner = null;
-  print(coutner);
+import 'dart:io';
 
-  // init
-  coutner = 0;
+int addTwoNumber(int firstNumber, int secondNumber) {
+  int result = firstNumber + secondNumber;
+  return result;
+}
 
-  var name = "hello";
-  print(name.runtimeType);
+int addTwoOrThreeNumber(int firstNumber, int secondNumber,
+    {int thirdNumber = 0}) {
+  int result = firstNumber + secondNumber + thirdNumber;
+  return result;
+}
 
-  var degree;
-  // ? String
-  degree = "Hello";
-  // ? int
-  degree = 30;
-  // ? NUll
-  degree = null;
-  // ? bool
-  degree = true;
+int addThreeNumber({
+  required int firstNumber,
+  required int secondNumber,
+  required int thirdNumber,
+}) {
+  int result = firstNumber + secondNumber + thirdNumber;
+  return result;
+}
 
-  Null empty = null;
-  print(empty);
+int result = 0;
 
-  num mark = 20;
-  print(mark.runtimeType);
-  mark = 20.4;
-  print(mark.runtimeType);
+void addThreeNumberAndVoid({
+  required int firstNumber,
+  required int secondNumber,
+  required int thirdNumber,
+}) {
+  firstNumber++;
 
-  dynamic anything = "Hello";
+  result = firstNumber + secondNumber + thirdNumber;
+  print(result);
+}
 
-  if (anything == "H") {
-    print(anything);
+scanner(String name) {
+  print("Dr . ${name}");
+  return name;
+}
+
+int hello(bool isFav) {
+  if (isFav) {
+    return 10;
   } else {
-    print("object");
+    throw Exception("HEllo Wolrd");
   }
-  // ! replace 30 with 30.5
-  num mathVa = 30;
+}
+
+void main(List<String> args) {
+  print(
+    addTwoNumber(
+      3,
+      5,
+    ),
+  );
+
+  print(
+    addTwoOrThreeNumber(
+      3,
+      5,
+      thirdNumber: 10,
+    ),
+  );
+
+  print(addThreeNumber(
+    firstNumber: 10,
+    secondNumber: 10,
+    thirdNumber: 9,
+  ));
+
+  addThreeNumberAndVoid(
+    firstNumber: 10,
+    secondNumber: 10,
+    thirdNumber: 10,
+  );
+  // ! 30 or 0 ? :
+  print(result);
+
+  print(scanner("Abd"));
+  dynamic data;
   try {
-    mathVa as double;
-    print("Hello");
+    data = hello(false);
   } catch (e) {
-    print("World");
-  }
-  // ? There is no exception divider by zero
-  double divider = 10 / 0;
-  print(divider);
-  divider = 9;
-  divider++;
-  print(divider);
-
-  for (var i = 0; i < 10; i++) {
-    print("Hello from ${i}");
+    print(e);
   }
 
-  List<dynamic> list = [123, 123.3, "Hello"];
-  print(list[0]);
-
-  Map<String, dynamic> marks = {
-    "name": "Ahmad",
-    "Math 3": 59,
-  };
-
-  print(marks.keys.elementAt(1));
-  print(marks["Math 3"]);
-
-  List nestedList = [
-    {
-      "id": 1,
-      "name": "Leanne Graham",
-      "username": "Bret",
-      "email": "Sincere@april.biz",
-      "address": {
-        "street": "Kulas Light",
-        "suite": "Apt. 556",
-        "city": "Gwenborough",
-        "zipcode": "92998-3874",
-        "geo": {"lat": "-37.3159", "lng": "81.1496"}
-      },
-      "phone": "1-770-736-8031 x56442",
-      "website": "hildegard.org",
-      "company": {
-        "name": "Romaguera-Crona",
-        "catchPhrase": "Multi-layered client-server neural-net",
-        "bs": "harness real-time e-markets"
-      }
-    },
-    {
-      "id": 2,
-      "name": "Ervin Howell",
-      "username": "Antonette",
-      "email": "Shanna@melissa.tv",
-      "address": {
-        "street": "Victor Plains",
-        "suite": "Suite 879",
-        "city": "Wisokyburgh",
-        "zipcode": "90566-7771",
-        "geo": {"lat": "-43.9509", "lng": "-34.4618"}
-      },
-      "phone": "010-692-6593 x09125",
-      "website": "anastasia.net",
-      "company": {
-        "name": "Deckow-Crist",
-        "catchPhrase": "Proactive didactic contingency",
-        "bs": "synergize scalable supply-chains"
-      }
-    },
-    {
-      "id": 3,
-      "name": "Clementine Bauch",
-      "username": "Samantha",
-      "email": "Nathan@yesenia.net",
-      "address": {
-        "street": "Douglas Extension",
-        "suite": "Suite 847",
-        "city": "McKenziehaven",
-        "zipcode": "59590-4157",
-        "geo": {"lat": "-68.6102", "lng": "-47.0653"}
-      },
-      "phone": "1-463-123-4447",
-      "website": "ramiro.info",
-      "company": {
-        "name": "Romaguera-Jacobson",
-        "catchPhrase": "Face to face bifurcated interface",
-        "bs": "e-enable strategic applications"
-      }
-    },
-    {
-      "id": 4,
-      "name": "Patricia Lebsack",
-      "username": "Karianne",
-      "email": "Julianne.OConner@kory.org",
-      "address": {
-        "street": "Hoeger Mall",
-        "suite": "Apt. 692",
-        "city": "South Elvis",
-        "zipcode": "53919-4257",
-        "geo": {"lat": "29.4572", "lng": "-164.2990"}
-      },
-      "phone": "493-170-9623 x156",
-      "website": "kale.biz",
-      "company": {
-        "name": "Robel-Corkery",
-        "catchPhrase": "Multi-tiered zero tolerance productivity",
-        "bs": "transition cutting-edge web services"
-      }
-    },
-    {
-      "id": 5,
-      "name": "Chelsey Dietrich",
-      "username": "Kamren",
-      "email": "Lucio_Hettinger@annie.ca",
-      "address": {
-        "street": "Skiles Walks",
-        "suite": "Suite 351",
-        "city": "Roscoeview",
-        "zipcode": "33263",
-        "geo": {"lat": "-31.8129", "lng": "62.5342"}
-      },
-      "phone": "(254)954-1289",
-      "website": "demarco.info",
-      "company": {
-        "name": "Keebler LLC",
-        "catchPhrase": "User-centric fault-tolerant solution",
-        "bs": "revolutionize end-to-end systems"
-      }
-    },
-    {
-      "id": 6,
-      "name": "Mrs. Dennis Schulist",
-      "username": "Leopoldo_Corkery",
-      "email": "Karley_Dach@jasper.info",
-      "address": {
-        "street": "Norberto Crossing",
-        "suite": "Apt. 950",
-        "city": "South Christy",
-        "zipcode": "23505-1337",
-        "geo": {"lat": "-71.4197", "lng": "71.7478"}
-      },
-      "phone": "1-477-935-8478 x6430",
-      "website": "ola.org",
-      "company": {
-        "name": "Considine-Lockman",
-        "catchPhrase": "Synchronised bottom-line interface",
-        "bs": "e-enable innovative applications"
-      }
-    },
-    {
-      "id": 7,
-      "name": "Kurtis Weissnat",
-      "username": "Elwyn.Skiles",
-      "email": "Telly.Hoeger@billy.biz",
-      "address": {
-        "street": "Rex Trail",
-        "suite": "Suite 280",
-        "city": "Howemouth",
-        "zipcode": "58804-1099",
-        "geo": {"lat": "24.8918", "lng": "21.8984"}
-      },
-      "phone": "210.067.6132",
-      "website": "elvis.io",
-      "company": {
-        "name": "Johns Group",
-        "catchPhrase": "Configurable multimedia task-force",
-        "bs": "generate enterprise e-tailers"
-      }
-    },
-    {
-      "id": 8,
-      "name": "Nicholas Runolfsdottir V",
-      "username": "Maxime_Nienow",
-      "email": "Sherwood@rosamond.me",
-      "address": {
-        "street": "Ellsworth Summit",
-        "suite": "Suite 729",
-        "city": "Aliyaview",
-        "zipcode": "45169",
-        "geo": {"lat": "-14.3990", "lng": "-120.7677"}
-      },
-      "phone": "586.493.6943 x140",
-      "website": "jacynthe.com",
-      "company": {
-        "name": "Abernathy Group",
-        "catchPhrase": "Implemented secondary concept",
-        "bs": "e-enable extensible e-tailers"
-      }
-    },
-    {
-      "id": 9,
-      "name": "Glenna Reichert",
-      "username": "Delphine",
-      "email": "Chaim_McDermott@dana.io",
-      "address": {
-        "street": "Dayna Park",
-        "suite": "Suite 449",
-        "city": "Bartholomebury",
-        "zipcode": "76495-3109",
-        "geo": {"lat": "24.6463", "lng": "-168.8889"}
-      },
-      "phone": "(775)976-6794 x41206",
-      "website": "conrad.com",
-      "company": {
-        "name": "Yost and Sons",
-        "catchPhrase": "Switchable contextually-based project",
-        "bs": "aggregate real-time technologies"
-      }
-    },
-    {
-      "id": 10,
-      "name": "Clementina DuBuque",
-      "username": "Moriah.Stanton",
-      "email": "Rey.Padberg@karina.biz",
-      "address": {
-        "street": "Kattie Turnpike",
-        "suite": "Suite 198",
-        "city": "Lebsackbury",
-        "zipcode": "31428-2261",
-        "geo": {"lat": "-38.2386", "lng": "57.2232"}
-      },
-      "phone": "024-648-3804",
-      "website": "ambrose.net",
-      "company": {
-        "name": "Hoeger LLC",
-        "catchPhrase": "Centralized empowering task-force",
-        "bs": "target end-to-end models"
-      }
-    }
-  ];
-
-  
+   // ! Bug vs Exception vs Failed vs Failure and Error // 
 }
