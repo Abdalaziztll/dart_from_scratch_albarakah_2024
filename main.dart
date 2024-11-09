6595dab6ed189c2f6c063fb9823b0f7efac36259
AbdTll <abdalaziztll992010@gmail.com>
2024-11-09 06:33:32 -0800

    future async await

--- main.dart
+++ main.dart
@@ -1,88 +1,66 @@
-import 'dart:io';
+Future<String> getData() async {
+  await Future.delayed(Duration(seconds: 2));
 
-int addTwoNumber(int firstNumber, int secondNumber) {
-  int result = firstNumber + secondNumber;
-  return result;
+  return "Hello";
 }
 
-int addTwoOrThreeNumber(int firstNumber, int secondNumber,
-    {int thirdNumber = 0}) {
-  int result = firstNumber + secondNumber + thirdNumber;
-  return result;
+Future<void> printAnyThing() async {
+  await Future.delayed(Duration(seconds: 5));
+
+  print("object");
 }
 
-int addThreeNumber({
-  required int firstNumber,
-  required int secondNumber,
-  required int thirdNumber,
-}) {
-  int result = firstNumber + secondNumber + thirdNumber;
-  return result;
-}
+main() async {
+  var name = await getData();
+  await printAnyThing();
+  print("World");
+  print(name);
 
-int result = 0;
+  final counter;
+  // ? Final Can not be used before it assigned
+  // print(counter);
+  counter = "20";
 
-void addThreeNumberAndVoid({
-  required int firstNumber,
-  required int secondNumber,
-  required int thirdNumber,
-}) {
-  firstNumber++;
+  // ? Can be set once
+  // counter = 20;
 
-  result = firstNumber + secondNumber + thirdNumber;
-  print(result);
-}
+  // ? Must be delcarated and Initilized
+  const age = 20;
 
-scanner(String name) {
-  print("Dr . ${name}");
-  return name;
-}
+  // age = 30;
 
-int hello(bool isFav) {
-  if (isFav) {
-    return 10;
-  } else {
-    throw Exception("HEllo Wolrd");
-  }
-}
+  Map<String, dynamic> map = {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {"lat": "-37.3159", "lng": "81.1496"}
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  };
 
-void main(List<String> args) {
-  print(
-    addTwoNumber(
-      3,
-      5,
-    ),
-  );
+  print(map['address']['geo']['lat']);
 
-  print(
-    addTwoOrThreeNumber(
-      3,
-      5,
-      thirdNumber: 10,
-    ),
-  );
+  print(map.values.elementAt(4).values.elementAt(4).values.elementAt(0));
 
-  print(addThreeNumber(
-    firstNumber: 10,
-    secondNumber: 10,
-    thirdNumber: 9,
-  ));
-
-  addThreeNumberAndVoid(
-    firstNumber: 10,
-    secondNumber: 10,
-    thirdNumber: 10,
-  );
-  // ! 30 or 0 ? :
-  print(result);
-
-  print(scanner("Abd"));
-  dynamic data;
-  try {
-    data = hello(false);
-  } catch (e) {
-    print(e);
-  }
-
-   // ! Bug vs Exception vs Failed vs Failure and Error // 
+  Map<String, List<int>> list = {
+    "hello": [
+      20,
+      40,
+      50,
+    ]
+  };
+  // ? Why there is an error ( foreach Student )// 
+  print(list["hello"][0]);
 }
